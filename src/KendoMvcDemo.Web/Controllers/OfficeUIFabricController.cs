3c73edff37460ddba02e6562dd3639f6ecb1e45b
pluksza <luksza.piotr@gmail.com>
2018-01-09 23:04:08 +0100

    add controller for office ui fabric

---
+++ src/KendoMvcDemo.Web/Controllers/OfficeUIFabricController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoMvcDemo.Web.Controllers
+{
+    public class OfficeUIFabricController : Controller
+    {
+        // GET: OfficeUIFabric
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
