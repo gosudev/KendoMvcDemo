2c1b5eade2dae29cd55faa04d24731442057499f
pluksza <luksza.piotr@gmail.com>
2018-01-09 10:07:13 +0100

    add simple complaint service

---
+++ src/KendoMvcDemo.Core/Services/ComplaintService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KendoMvcDemo.Core.ViewModels;
+
+namespace KendoMvcDemo.Core.Services
+{
+    public class ComplaintService
+    {
+        public IList<ComplaintViewModel> GetAll()
+        {
+            return Enumerable.Range(1, 50).Select(x => new ComplaintViewModel()
+            {
+                Id = x,
+                Title = $"Complaint nr: {x}",
+                ConsumerNarrative = $"{x} Lorem ipsum dolor sit amet,sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,  At vero eos et accusam et justo duo dolores et ea rebum.  Lorem ipsum dolor sit amet,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  Stet clita kasd gubergren,  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  no sea takimata sanctus est Lorem ipsum dolor sit amet.  sed diam voluptua.  ",
+                SentDate = DateTime.Now.AddDays(-x)
+
+            }).ToList();
+        }
+    }
+}
---
+++ src/KendoMvcDemo.Core/ViewModels/ComplaintViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KendoMvcDemo.Core.ViewModels
+{
+    public class ComplaintViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string ConsumerNarrative { get; set; }
+        public DateTime SentDate { get; set; }
+    }
+}
