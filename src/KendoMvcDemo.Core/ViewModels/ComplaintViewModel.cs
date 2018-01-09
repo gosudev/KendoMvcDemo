8475910ee526583c8924d976865fcf3d0331f29f
pluksza <luksza.piotr@gmail.com>
2018-01-09 10:18:09 +0100

    extend complaintviewmodel

--- src/KendoMvcDemo.Core/ViewModels/ComplaintViewModel.cs
+++ src/KendoMvcDemo.Core/ViewModels/ComplaintViewModel.cs
@@ -10,7 +10,10 @@ namespace KendoMvcDemo.Core.ViewModels
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string ConsumerNarrative { get; set; }
+        public string WhatHappend { get; set; }
+        public string Company { get; set; }
         public DateTime SentDate { get; set; }
+
+        public int ProductId { get; set; }
     }
 }
